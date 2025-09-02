5f48dc23537eb30404af887e356dee73a3dcbd73
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-09-02 22:50:29 +0700

    Added link shortening functionality with input and delete options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+
+const shortenLinks = {
+  "https://www.example.com/very/long/link": "https://short.ly/abc123",
+  "https://www.anotherexample.com/some/other/long/link":
+    "https://short.ly/def456",
+  "https://www.yetanotherexample.com/a/different/long/link":
+    "https://short.ly/ghi789",
+};
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <section className="lg:container lg:m-auto">
+      <section className="lg:container lg:m-auto pb-10 md:pb-20 lg:pb-24">
         {" "}
         <div className="lg:px-8">
           <div className="overflow-hidden md:flex md:items-center md:justify-center md:flex-row-reverse my-8 m-auto w-full">
@@ -34,9 +42,40 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section></section>
-      <section className="bg-gray-100 min-h-[200px]">
-        <div className="container m-auto text-center py-16 px-4">
+      <section className="bg-gray-100 px-4">
+        <div className="-translate-y-14 mx-auto lg:-translate-y-8">
+          <div className="container flex items-center justify-center mx-auto px-4 py-5 bg-[url(/bg-shorten-mobile.svg)] lg:bg-[url(/bg-shorten-desktop.svg)] bg-no-repeat rounded-lg gap-4 flex-col lg:flex-row lg:gap-2 bg-purple-95 lg:w-3xl lg:px-8">
+            <input
+              type="text"
+              className="bg-white placeholder:text-gray-4 px-2 py-1 rounded w-full italic"
+              placeholder="Shorten a link here..."
+            />
+            <button
+              type="button"
+              className="bg-blue-4 hover:scale-99 text-white font-bold py-1 px-4 rounded transition-transform duration-200 w-full cursor-pointer lg:w-1/3"
+            >
+              Shorten It!
+            </button>
+          </div>
+          <div className="container mt-3 rounded-lg flex flex-col gap-3 lg:w-3xl mx-auto md:w-3xl">
+            {Object.entries(shortenLinks).map(([long, short]) => (
+              <div
+                key={long}
+                className="flex flex-col items-center justify-between bg-white rounded-lg p-4 shadow gap-2"
+              >
+                <span className="text-gray-6">{long}</span>
+                <span className="text-blue-4">{short}</span>
+                <button
+                  type="button"
+                  className="bg-blue-4 text-white py-1 px-4 rounded hover:scale-99 transition-transform duration-200"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="container m-auto text-center py-6 px-4">
           <h2 className="mb-4 font-bold text-3xl">Advanced Statistics</h2>
           <p className="text-gray-5 max-w-lg m-auto text-sm">
             Track how your links are performing with our advanced statistics
