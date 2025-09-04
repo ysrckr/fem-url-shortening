013ef0f3e6282cf090ca44180393213da952cc2d
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-09-04 20:34:30 +0700

    main: Moved card data to a separate module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,37 +5,8 @@ import Link from "next/link";
 import { Shortener } from "@/components/shortener/shortener";
 import { ShortenerSkeleton } from "@/components/skeletons/shortener.skeleton";
 import { Suspense } from "react";
+import { cards } from "../data/cards";
 import { cn } from "@/utils/cn";
-
-const cards = [
-  {
-    title: "Brand Recognition",
-    content:
-      "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.",
-    image: {
-      src: "/icon-brand-recognition.svg",
-      alt: "Brand Recognition",
-    },
-  },
-  {
-    title: "Detailed Records",
-    content:
-      "Gain insights into your link performance with our detailed records. Understand your audience better and optimize your strategies.",
-    image: {
-      src: "/icon-detailed-records.svg",
-      alt: "Detailed Records",
-    },
-  },
-  {
-    title: "Fully Customizable",
-    content:
-      "Improve brand awareness and content discoverability through customizable links, landing pages, and more.",
-    image: {
-      src: "/icon-fully-customizable.svg",
-      alt: "Fully Customizable",
-    },
-  },
-];
 
 export default function Home() {
   return (
