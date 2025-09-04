fe42444843eba6d324b8722e5b0085f54ae596b1
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-09-04 20:31:34 +0700

    Refactor ShortenerForm component to improve error handling and state management; replace direct state mutation with useState for showErrors

--- src/components/shortener/shortener-form.tsx
+++ src/components/shortener/shortener-form.tsx
@@ -1,41 +1,49 @@
+"use client";
+
+import { useActionState, useEffect, useState } from "react";
+
 import { Button } from "../ui/button";
 import { cn } from "@/utils/cn";
 import { shortenURL } from "@/app/actions";
-import { useActionState } from "react";
-
-const initialState = { errors: [] as string[], shortURL: undefined };
 
 export const ShortenerForm = () => {
+  const initialState = { errors: [] as string[], shortURL: undefined };
   const [state, formAction, pending] = useActionState(shortenURL, initialState);
+  const [showErrors, setShowErrors] = useState(false);
 
-  const clearErrors = () => {
-    if (state.errors.length > 0) {
-      state.errors = [];
-    }
-  };
+  useEffect(() => {
+    if (pending) return;
+    setShowErrors(state.errors.length > 0);
+  }, [state.errors.length, pending]);
+
   return (
-    <form>
-      <div className="container flex items-center justify-center mx-auto px-4 py-5 bg-[url(/bg-shorten-mobile.svg)] lg:bg-[url(/bg-shorten-desktop.svg)] bg-no-repeat rounded-lg gap-4 flex-col lg:flex-row lg:gap-2 bg-purple-95 lg:w-3xl lg:px-8">
+    <form action={formAction}>
+      <div className="container flex items-center justify-center mx-auto px-4 py-5 bg-[url(/bg-shorten-mobile.svg)] lg:bg-[url(/bg-shorten-desktop.svg)] bg-no-repeat rounded-lg gap-4 flex-col lg:flex-row lg:gap-2 bg-purple-95 lg:w-3xl lg:px-8 relative">
         <input
           type="text"
           className={cn(
             "bg-white placeholder:text-gray-4 px-2 py-1 rounded w-full placeholder:italic focus:outline-none lg:py-2 lg:px-4 lg:text-lg",
             {
-              "border-2 border-red-500": state.errors.length > 0,
+              "border-2 border-red-500": showErrors,
             }
           )}
           placeholder="Shorten a link here..."
           name="longURL"
-          onChange={clearErrors}
-          aria-invalid={state.errors.length > 0}
-          aria-describedby={
-            state.errors.length > 0 ? "error-message" : undefined
-          }
+          aria-invalid={showErrors}
+          aria-describedby={showErrors ? "error-message" : undefined}
+          disabled={pending}
+          onChange={() => setShowErrors(false)}
+          required
         />
-        {state.errors.length > 0 && (
-          <span className="text-red-500 italic">{state.errors.join(", ")}</span>
+        {showErrors && (
+          <span
+            className="text-red-500 italic absolute bottom-0 left-0 translate-x-4 max-lg:-translate-y-12 text-sm lg:translate-x-8"
+            id="error-message"
+          >
+            {state.errors.join(", ")}
+          </span>
         )}
-        <Button className="lg:w-1/3" formAction={formAction} disabled={pending}>
+        <Button className="lg:w-1/3" disabled={pending} type="submit">
           <span
             className={cn({
               "animate-pulse": pending,
